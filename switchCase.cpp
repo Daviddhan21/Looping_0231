927b7eb7101cde36e7c40af12b8af48f8b335181
Daviddhan21 <rayvanza.davida.ft24@mail.umy.ac.id>
2026-03-30 20:37:17 +0700

    Perbaiki judul output hasil seleksi

--- switchCase.cpp
+++ switchCase.cpp
@@ -46,7 +46,7 @@ void pilihanJurusan() {
 // prosedur output
 // menampilkan hasil seleksi jurusan
 void output() {
-    cout << "\n=== HASIL SELEKSI ===" << endl;
+    cout << "\n=== HASIL SELEKSI JURUSAN ===" << endl;
     cout << "Jurusan Pilihan : " << namaJurusan << endl;
     cout << "Status Kuota    : " << statusKuota << endl;
     cout << "-----------------------------------" << endl;
