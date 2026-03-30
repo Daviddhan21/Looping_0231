52892d043220cf028a01b97dd1a74feb0d4cb5f8
Daviddhan21 <rayvanza.davida.ft24@mail.umy.ac.id>
2026-03-30 20:20:49 +0700

    Initial commit

---
+++ looping.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main(){
+    int x;
+
+    // Perulangan dengan FOR
+    cout<<"PERULANGAN FOR"<<endl;
+    for(int i=1; i<=4; i++){
+        cout<<"Teknologi Informasi UMY"<<endl;
+    }
+    cout<<endl;
+
+    // untuk mendapatkan urutan angka acak yang berbeda
+    srand(time(0));
+
+    // Perulangan dengan WHILE
+    cout<<"PERULANGAN WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    while(x < 5){
+        cout<<"Bilangan acak = "<<x<<endl;
+        x = rand()%10;
+    }
+
+    cout<<"Bilangan acak while yang terakhir = "<<x<<endl;
+    cout<<endl;
+
+    // Perulangan dengan DO WHILE
+    cout<<"PERULANGAN DO...WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    do{
+        cout<<"Bilangan acak = "<<x<<endl;
+        x = rand()%10;
+    }while(x < 5);
+
+    cout<<"Bilangan acak do-while yang terakhir = "<<x<<endl;
+    cout<<endl;
+
+}
